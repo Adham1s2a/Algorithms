9c7d807dd00793d6b002941eb49f6a84bfe609de
Adham Moamer <Adham1s2a@gmail.com>
2021-09-04 16:50:27 +0300

    Update Factorial.ts

--- Recursion/Factorial.ts
+++ Recursion/Factorial.ts
@@ -2,7 +2,6 @@
 // Loops may achieve a performance gain for
 // your program. Recursion may achieve a performance gain for your
 // programmer. Choose which is more important in your situation!”
-
 //Leigh Caldwell
 
 
